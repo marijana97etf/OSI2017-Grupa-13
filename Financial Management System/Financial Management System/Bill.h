a65f02b137699147c810dfda55c98e3a119c671b
baralic david <baralicdavid97@gmail.com>
2017-12-19 12:02:29 +0100

    dodati proptotipovi potrebni podaci u klasi bill

--- Financial Management System/Financial Management System/Bill.h
+++ Financial Management System/Financial Management System/Bill.h
@@ -1,5 +1,16 @@
 #pragma once
 #include "Product.h"
+#include <Windows.h>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#define log "Log.txt"
+#define logError "logError.txt"
+
+bool isProcessedBill(const std::string);
+std::vector<std::string> returnVectorOfNotProcessedBills(const std::string directory);
+bool checkFormat1(const std::string);
 
 class Bill
 {
@@ -9,27 +20,23 @@ class Bill
 		Node *next;
 
 		Node(const Product&);
-		Node();
 	};
 	struct Date
 	{
 		int day, month, year;
 		Date(const int, const int, const int);
-		Date() = default;
 	};
 	Node* head,*tail;
 	std::string nameOfBill;
-	double totalSumOfProducts,pdv,totalSumOfBill;
 	Date date;
 public:
+	Bill();
 	Bill(const std::string &);//daje se ime .txt ili .csv koji se treba obraditi
 	void procces();//jedna od vecih funkcija koja treba da iz fajla sve podatke izdvoji u listu
 	void Validate();//izvrsava validaciju podataka
 	bool checkTotalOfEveryProduct();
 	bool checkTotalofAllproducts();
 	bool checkTotalPlusPDV();
-
-
 	~Bill();
 };
 
