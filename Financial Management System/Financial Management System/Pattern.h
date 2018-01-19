79b07593efa623684192df804cbbc4ab5a730b51
baralic david <baralicdavid97@gmail.com>
2018-01-19 23:49:21 +0100

    neke male izmjene

--- Financial Management System/Financial Management System/Pattern.h
+++ Financial Management System/Financial Management System/Pattern.h
@@ -6,22 +6,21 @@ namespace patterns
 	const int MAX_OPTIONS_FOR_ANALYST = 5;
 	const std::string optionsForAnalist[5] =
 	{
-		"[1]Pregled za odredjenog korisnika",
-		"[2]Pregled za odredjeni proizvod",
-		"[3]Pregled za odredjeni mjesec",
-		"[0]Odjava",
-		"[-1]Izlaz iz programa"
+		"[1] Pregled za odredjenog korisnika",
+		"[2] Pregled za odredjeni proizvod",
+		"[3] Pregled za odredjeni mjesec",
+		"[0] Odjava",
+		"[-1] Izlaz iz programa"
 	};
 	const int MAX_OPTIONS_FOR_ADMIN = 8;
 	const std::string optionsForAdmin[8] =
 	{
-		"[1]Dodaj odredjeni nalog",
-		"[2]Obrisi odredjeni nalog",
-		"[3]Ispisi naloge na sistemu",
-		"[4]Izmjeni odredjeni nalog",
-		"[5]Promjena korisnickog interfejsa",
-		"[6]Unos valute sistema",
-		"[0]Odjava",
-		"[-1]Izlaz iz programa"
+		"[1] Dodaj odredjeni nalog",
+		"[2] Obrisi odredjeni nalog",
+		"[3] Ispisi naloge na sistemu",
+		"[4] Izmjeni odredjeni nalog",
+		"[5] Promjena korisnickog interfejsa",
+		"[0] Odjava",
+		"[-1] Izlaz iz programa"
 	};
 }
