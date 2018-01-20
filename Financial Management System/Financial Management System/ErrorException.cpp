c23fc68d59412641f76e9586e9fd4ae573f9f98d
Saša Šukalo <sasasukalo@yahoo.com>
2018-01-20 21:30:13 +0100

    Ispravka snimanja formata5  .csv  ako je pogresan

--- Financial Management System/Financial Management System/ErrorException.cpp
+++ Financial Management System/Financial Management System/ErrorException.cpp
@@ -8,10 +8,11 @@ ErrorException::ErrorException(const std::string& file, const std::string& messa
 
 void ErrorException::processException()
 {
-		std::ofstream error(file.substr(0, file.length() - 4) + "_error.txt", std::ios::app | std::ios::out);//ako se ne moze dodati onda napravi novi fajl
-		error << message << std::endl;
-		std::ofstream logErrorOut(LOGERROR, std::ios::app | std::ios::out );
-		logErrorOut << file << std::endl;
+	std::string temp = file.substr(file.length() - 4, 4);
+	std::ofstream error(file.substr(0, file.length() - 4) + "_error" + temp, std::ios::app | std::ios::out);//ako se ne moze dodati onda napravi novi fajl
+	error << message << std::endl;
+	std::ofstream logErrorOut(LOGERROR, std::ios::app | std::ios::out );
+	logErrorOut << file << std::endl;
 }
 
 ErrorException::~ErrorException()
