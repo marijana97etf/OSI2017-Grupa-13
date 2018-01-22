1e09b2f659b4617f1494d1abc6a2d91f974dab01
Kristijan Stepanov <stepanov@hotmail.rs>
2018-01-22 01:07:45 +0100

    Komentarisanje metoda

--- Financial Management System/Financial Management System/Bill.h
+++ Financial Management System/Financial Management System/Bill.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Product.h"
 #include "Account.h"
 #include "ErrorException.h"
@@ -16,15 +16,15 @@ const int isFormat[5] = { 1,2,3,4,5 } ;
 const std::string month[12] = { "Januar","Februar","Mart","April","Maj","Jun","Jul","Avgust","Septembar","Oktobar","Novembar","Decembar" };
 const double EPS = 0.5;
 
-bool isProcessedBill(const std::string);
-std::vector<std::string> returnVectorOfNotProcessedBills(const std::string directory);
-bool checkFormat1(const std::string);
-bool checkFormat2(const std::string);
-bool checkFormat3(const std::string);
-bool checkFormat4(const std::string);
-bool checkFormat5(const std::string);
+bool isProcessedBill(const std::string);      //	funkcija koja provjerava da li je račun procesiran
+std::vector<std::string> returnVectorOfNotProcessedBills(const std::string directory); //   funkcija koja vraća niz računa koji nisu procesirani
+bool checkFormat1(const std::string);         //
+bool checkFormat2(const std::string);		  //
+bool checkFormat3(const std::string);		  //    funkcije za procesiranje svih 5 formata računa
+bool checkFormat4(const std::string);		  //
+bool checkFormat5(const std::string);         //
 
-class Bill
+class Bill			// Klasa "Račun"
 {
 	struct Date
 	{
@@ -33,34 +33,34 @@ class Bill
 		Date() = default;
 		Date(const int, const int, const int);
 	};
-	std::list <Product> list;
+	std::list <Product> list;         
 	std::string nameOfBill,nameOfClient;
 	double totalSumOfProducts, pdv, totalSumOfBill;
 	int formatCode;
 	Date date;
 public:
-	Bill();
-	Bill(const std::string &,int);
-	void process();
-	bool Validate();
-	void exportForCustomer();
-	void exportForProduct();
-	void exportForMonth();
+	Bill();											 // podrazumjevani konstruktor
+	Bill(const std::string &,int);					 // konstruktor
+	void process();									 // procesiranje računa
+	bool Validate();								 // validacija računa
+	void exportForCustomer();						 // izvoz podataka za kupca
+	void exportForProduct();					   	 // izvoz podataka za proizvod
+	void exportForMonth();		              	     // izvoz podataka za mjesec
 	~Bill();
 private:
-	bool checkTotalOfEveryProduct();
-	bool checkTotalofAllproducts();
-	bool checkTotalPlusPDV();
-	void processFormat1();
-	void processFormat2();
-	void processFormat3();
-	void processFormat4();
-	void processFormat5();
-	void processDate(std::ifstream &);
-	Product processDataForFormat124(std::string &);
-	Product processDataForFormat3(std::string &);
-	void putNewProductInList(const Product&);
-	void ignoreElementsUntil(std::ifstream &, char);
-	int numberOfDigits(double);
+	bool checkTotalOfEveryProduct();	  		     // metoda za provjeru ukupne cijene svakog proizvoda
+	bool checkTotalofAllproducts();				     // metoda za provjeru ukupne cijene svih proizvoda
+	bool checkTotalPlusPDV();					     // metoda za provjeru ispravnosti konačne cijene (Ukupno+PDV)
+	void processFormat1();							 // metoda za provjeru formata prvog tipa računa
+	void processFormat2();							 // metoda za provjeru drugog tipa računa
+	void processFormat3();							 // metoda za provjeru treceg tipa računa
+	void processFormat4();							 // metoda za provjeru četvrtog tipa računa
+	void processFormat5();							 // metoda za provjeru petog tipa računa
+	void processDate(std::ifstream &);				 // metoda za procesiranje datuma
+	Product processDataForFormat124(std::string &);  // metoda za procesiranje podataka za formate računa 1, 2 i 4
+	Product processDataForFormat3(std::string &);    // metoda za procesiranje podataka za račun sa trećim formatom računa
+	void putNewProductInList(const Product&);        // metoda za dodavanje proizvoda u listu
+	void ignoreElementsUntil(std::ifstream &, char); // metoda za ignorisanje karaktera 
+	int numberOfDigits(double);						 // metoda koja vraća broj cifara broja
 };
 
